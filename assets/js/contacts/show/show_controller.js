c4b388e77d385ae1aae22613132b9e57b3b7d39c
Koen Castermans <castermanskoen@msn.com>
2017-03-21 16:06:15 +0100

    Added loading view for showing a contact

--- assets/js/contacts/show/show_controller.js
+++ assets/js/contacts/show/show_controller.js
@@ -3,6 +3,10 @@ ContactManager.module('ContactsApp.Show', function (Show, ContactManager, Backbo
     //Show controller that manages our show view for a contact
     Show.Controller = {
         showContact: function (id) {
+            //Artificial loading view to show the user that something is happening
+            var loadingView = new ContactManager.Common.Views.Loading();
+            ContactManager.regions.main.show(loadingView);
+
             var fetchingContact = ContactManager.request('contact:entity', id);
 
             //Wait for the promise to return the data.
